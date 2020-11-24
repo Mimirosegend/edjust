f938f8f669791ab269d53e91dfb4ac2519617adc
Mimi Gendreau Kigawa <mimigk@Mimis-MBP.fios-router.home>
2020-11-23 19:24:27 -0500

    make platform functional

--- src/platform_district/platform_district.js
+++ src/platform_district/platform_district.js
@@ -6,30 +6,30 @@ import {
 } from "react-router-dom";
 
 let sel;
-
-function setup() {
-  textAlign(CENTER);
-  background(200);
-  sel = createSelect();
-  sel.position(10, 10);
-  sel.option('pear');
-  sel.option('kiwi');
-  sel.option('grape');
-  sel.selected('kiwi');
-  sel.changed(mySelectEvent);
-}
-
-function mySelectEvent() {
-  let item = sel.value();
-  background(200);
-  text('It is a ' + item + '!', 50, 50);
-}
+//
+// function setup() {
+//   textAlign(CENTER);
+//   background(200);
+//   sel = createSelect();
+//   sel.position(10, 10);
+//   sel.option('pear');
+//   sel.option('kiwi');
+//   sel.option('grape');
+//   sel.selected('kiwi');
+//   sel.changed(mySelectEvent);
+// }
+//
+// function mySelectEvent() {
+//   let item = sel.value();
+//   background(200);
+//   text('It is a ' + item + '!', 50, 50);
+// }
 
 class Platform_District extends React.Component {
   render() {
     return (
       <div>
-        
+      HELLO
       </div>
     )
   }
